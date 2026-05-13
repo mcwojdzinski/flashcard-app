bca4cbf37ad117645908a7f134d0a11e2875a9cf
mcwojdzinski <mateuszcwojdzinski@outlook.com>
2026-05-13 15:10:55 +0200

    Add Logo and Input component into the project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Logo } from './components/logo/logo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Logo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
