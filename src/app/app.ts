4ed506ea094deaa0880e1cebb68d3ef6c31ca1f2
mcwojdzinski <mateuszcwojdzinski@outlook.com>
2026-05-18 15:27:51 +0200

    feat: Show all components on the app

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Logo } from './components/logo/logo';
 import { Input } from './components/input/input';
 import { Textarea } from './components/textarea/textarea';
+import { Button } from "./components/button/button";
+import { ProgressBar } from "./components/progress-bar/progress-bar";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Logo, Input, Textarea],
+  imports: [RouterOutlet, Logo, Input, Textarea, Button, ProgressBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
