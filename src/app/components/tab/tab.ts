7f166d1fb163c0bcf477316b29e6b7158620d762
mcwojdzinski <mateuszcwojdzinski@outlook.com>
2026-05-19 11:40:14 +0200

    feat: Add tab component

---
+++ src/app/components/tab/tab.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { TabButtonBase } from "../tab-button-base/tab-button-base";
+
+@Component({
+  selector: 'app-tab',
+  imports: [TabButtonBase],
+  templateUrl: './tab.html',
+  styleUrl: './tab.css',
+})
+export class Tab {}
