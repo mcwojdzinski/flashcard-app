3f450cbf196942cdc0d7f4b25f2a0bbbe19ffa0a
mcwojdzinski <mateuszcwojdzinski@outlook.com>
2026-05-21 12:42:24 +0200

    feat: Add styling with animation for flashcard content with category button

--- src/app/views/study-mode-component/flashcard-section/flashcard-container/flashcard-content/flashcard-content.ts
+++ src/app/views/study-mode-component/flashcard-section/flashcard-container/flashcard-content/flashcard-content.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 import { ProgressBar } from "../../../../../components/shared/progress-bar/progress-bar";
 import { Tag } from "../../../../../components/shared/tag/tag";
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-flashcard-content',
-  imports: [ProgressBar, Tag],
+  imports: [ProgressBar, Tag, NgClass],
   templateUrl: './flashcard-content.html',
   styleUrl: './flashcard-content.css',
 })
-export class FlashcardContent {}
+export class FlashcardContent {
+  isRevealed = signal(false);
+
+  toggle() {
+    this.isRevealed.update(v => !v);
+  }
+
+  
+  @HostListener('document:keyup.space', ['$event'])
+  handleSpace(event: KeyboardEvent | Event) {
+    (event as KeyboardEvent).preventDefault();
+    this.toggle();
+  }
+
+}
--- src/app/views/study-mode-component/flashcard-section/flashcard-section.ts
+++ src/app/views/study-mode-component/flashcard-section/flashcard-section.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Button } from "../../../components/shared/button/button";
 import { Checkbox } from "../../../components/shared/checkbox/checkbox";
 import { FlashcardContainer } from "./flashcard-container/flashcard-container";
+import { CategoryFilter } from "../../../components/shared/category-filter/category-filter";
 
 @Component({
   selector: 'app-flashcard-section',
-  imports: [Button, Checkbox, FlashcardContainer],
+  imports: [Button, Checkbox, FlashcardContainer, CategoryFilter],
   templateUrl: './flashcard-section.html',
   styleUrl: './flashcard-section.css',
 })
