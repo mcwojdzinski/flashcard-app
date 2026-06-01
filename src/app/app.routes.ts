71c3657e76fa9d7bf45c2ed417cd2d35e7d3c72b
mcwojdzinski <mateuszcwojdzinski@outlook.com>
2026-06-01 12:47:08 +0200

    feat: Add router paths with authentication

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,31 @@
 import { Routes } from '@angular/router';
-import { ComponentsListView } from './views/components-list-view/components-list-view';
 import { StudyModeComponent } from './views/study-mode-component/study-mode-component';
 import { AllCardsComponent } from './views/all-cards-component/all-cards-component';
+import { LoginComponent } from './views/login-component/login-component';
+import { RegisterComponent } from './views/register-component/register-component';
+import { AuthGuard } from './services/auth.guard';
 
 export const routes: Routes = [
-    {
-        path: '', component: StudyModeComponent
-    },
-    {
-        path: 'all-cards', component: AllCardsComponent
-    },
-    {
-        path: 'components-list', component: ComponentsListView
-    }
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: '',
+    component: StudyModeComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'all-cards',
+    component: AllCardsComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,27 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, inject, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { Logo } from './components/shared/logo/logo';
 import { Tab } from "./components/shared/tab/tab";
+import { Button } from './components/shared/button/button';
 import { Dialog } from './components/shared/dialog/dialog';
 import { Toast } from './components/shared/toast/toast';
+import { AuthService } from './services/auth.service';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Logo, Tab, Dialog, Toast],
+  imports: [RouterOutlet, Logo, Tab, Button, Dialog, Toast],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  private authService = inject(AuthService);
+  private router = inject(Router);
   protected readonly title = signal('flashcard-app');
+  protected readonly isAuthenticated = computed(() => !!this.authService.token());
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
